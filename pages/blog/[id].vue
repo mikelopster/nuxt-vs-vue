fd4d5c8d016713ca8b34cb88c473294964f6b7bd
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2023-10-02 16:53:41 +0700

    add init project

---
+++ pages/blog/[id].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute()
+
+console.log(route.params.id)
+</script>
+
+<template>
+  <div>{{ $route.params.id }}</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import blogs from '~/data/blogs.json'
+import { ref } from 'vue'
+
+const firstname = ref('')
+
+useHead({
+  title: 'Mikelopster App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      Firstname {{ firstname }}
+      <input v-model="firstname">
+    </div>
+    <h1>Blog List</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        <nuxt-link :to="`/blog/${blog.id}`">{{ blog.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
